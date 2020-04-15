d8bf330d29ca3e5aba2cc8cfecfb6daf4963e6df
c14180045 <c14180045@john.petra.ac.id>
2020-04-15 09:22:10 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  output = 0;
+  calculate(input1, input2){
+  this.output = Math.pow(parseInt(input1),parseInt(input2))
+  } 
 }
+
+
